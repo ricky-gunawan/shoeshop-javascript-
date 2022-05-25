44f55e511c47975b2a35053dc6a0510aeee5fa9e
Ricky Indra Gunawan <rickyindra006@gmail.com>
2022-05-25 11:12:27 +0700

    edit filters component

--- frontend/src/components/Filters.js
+++ frontend/src/components/Filters.js
@@ -23,7 +23,73 @@ export default function Filters() {
         <button onClick={() => (filters ? setFilters(false) : setFilters(true))} className="absolute right-0 top-0 mt-2 -mr-2 translate-x-full rounded-md lg:hidden bg-cyan-400/50 text-neutral-50/50">
           {filters ? menuClose : menuOpen}
         </button>
-        <div>Filters</div>
+        <div className="m-2">
+          <span className="mx-auto block w-fit text-lg font-bold">Filters</span>
+          <hr className="border border-black" />
+          <form action="">
+            <div className="flex flex-col">
+              <span className="font-semibold m-2">Brand :</span>
+              <div className="flex flex-col ml-6">
+                <label className="flex items-center" htmlFor="adidas">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="brand" id="adidas" />
+                  <span className="ml-2">Adidas</span>
+                </label>
+                <label className="flex items-center" htmlFor="converse">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="brand" id="converse" />
+                  <span className="ml-2">Converse</span>
+                </label>
+                <label className="flex items-center" htmlFor="new_balance">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="brand" id="new_balance" />
+                  <span className="ml-2">New Balance</span>
+                </label>
+                <label className="flex items-center" htmlFor="nike">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="brand" id="nike" />
+                  <span className="ml-2">Nike</span>
+                </label>
+                <label className="flex items-center" htmlFor="puma">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="brand" id="puma" />
+                  <span className="ml-2">Puma</span>
+                </label>
+                <label className="flex items-center" htmlFor="reebok">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="brand" id="reebok" />
+                  <span className="ml-2">Reebok</span>
+                </label>
+                <label className="flex items-center" htmlFor="vans">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="brand" id="vans" />
+                  <span className="ml-2">Vans</span>
+                </label>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <span className="font-semibold m-2">Color :</span>
+              <div className="flex flex-col ml-6">
+                <label className="flex items-center" htmlFor="black">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="color" id="black" />
+                  <span className="ml-2">Black</span>
+                </label>
+                <label className="flex items-center" htmlFor="white">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="color" id="white" />
+                  <span className="ml-2">White</span>
+                </label>
+                <label className="flex items-center" htmlFor="red">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="color" id="red" />
+                  <span className="ml-2">Red</span>
+                </label>
+                <label className="flex items-center" htmlFor="blue">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="color" id="blue" />
+                  <span className="ml-2">Blue</span>
+                </label>
+                <label className="flex items-center" htmlFor="green">
+                  <input className="rounded-md text-cyan-400 focus:ring-1 focus:ring-cyan-400" type="radio" name="color" id="green" />
+                  <span className="ml-2">Green</span>
+                </label>
+              </div>
+            </div>
+            <a href="" className="flex justify-end">
+              <span className=" rounded-md bg-cyan-400 m-1 mt-2 px-2 py-1 font-semibold text-neutral-50 hover:scale-105">Submit</span>
+            </a>
+          </form>
+        </div>
       </div>
     </div>
   );
