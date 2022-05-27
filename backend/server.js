7df7cd1e9f4a37efcf5016771fd56ac621ecf21d
Ricky Indra Gunawan <rickyindra006@gmail.com>
2022-05-27 23:35:12 +0700

    test all route (get all, get one, create one, update one, and deleting one)

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ connectDB();
 
 app.use(express.json());
 
+// GET all products
 app.get("/api/products", async (req, res) => {
   try {
     const Products = await Product.find({});
@@ -24,22 +25,57 @@ app.get("/api/products", async (req, res) => {
     res.status(500).send(error);
   }
 });
-app.get("/api/products/:id", (req, res) => {
+
+//GET single product
+app.get("/api/products/:id", async (req, res) => {
   const params = req.params.id;
-  res.send(`test get one, id: ${params}`);
+  try {
+    const searchProduct = await Product.findById(params);
+    if (searchProduct) {
+      res.json(searchProduct);
+    } else {
+      res.status(404).send("couldn't find the product");
+    }
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
-app.post("/api/product", (req, res) => {
-  res.send(req.body);
+
+//PORT new product
+app.post("/api/product", async (req, res) => {
+  const newProduct = req.body;
+  try {
+    await Product.create(newProduct);
+    res.status(201).send("berhasil menambahkan product");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
-app.patch("/api/product/:id", (req, res) => {
+
+//PATCH updating product
+app.patch("/api/product/:id", async (req, res) => {
   const params = req.params.id;
-  res.send(`test patch, id: ${params}`);
+  const updatedProduct = req.body;
+
+  try {
+    await Product.findByIdAndUpdate(params, updatedProduct);
+    res.send("Product updated");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
-app.delete("/api/product/:id", (req, res) => {
+
+//DELETE product
+app.delete("/api/product/:id", async (req, res) => {
   const params = req.params.id;
-  res.send(`test delete, id: ${params}`);
+  try {
+    await Product.findByIdAndDelete(params);
+    res.status(201).send("succeed deleting product");
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+  console.log(`server is running on port: ${port}`);
 });
